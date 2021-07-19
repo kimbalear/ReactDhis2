564315dfc903dcffcc572d761f2be3220bf59edc
kimbalear <kimbalear@gmail.com>
2021-07-19 14:02:37 +0200

    BarSlctr

--- src/assets/pages/program_list/programlist_pg.js
+++ src/assets/pages/program_list/programlist_pg.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import ProgramSlctr from '../../../components/bar_slctr/bar_slctr';
+import BarSlctr from '../../../components/bar_slctr/bar_slctr';
 class ProgramList extends React.Component {
  render() {
     return (
-            <ProgramSlctr/>
+            <BarSlctr/>
       );
  }
 }
