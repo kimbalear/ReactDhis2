10b23844e1d61b4e52b6950ff55ecf1dee24bbca
kimbalear <kimbalear@gmail.com>
2021-07-26 11:45:59 +0200

    Router,   Switch,   Route,

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 //import './assets/css/App.css';
 import ProgramList from './assets/pages/ProgramList'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 
 function App() {
   return (
-      <ProgramList />
+    <Router>
+
+      <Switch>
+        <Route path="/" exact>
+          Start
+        </Route>
+        <Route path="/programs">
+          <ProgramList />
+        </Route>
+        <Route path="/program_stages">
+          Program Stages
+        </Route>
+        <Route path="/program_stage">
+          Program Stage
+        </Route>
+        <Route path="/components">
+          Components
+        </Route>
+      </Switch>
+    </Router>
+
   );
 }
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom';
 import './assets/scss/mysass.scss';
 //import ProgramList from './assets/pages/ProgramList'
 import App from'./App';
+
 const container = document.getElementById('root')
 ReactDOM.render( <App/>, container);
